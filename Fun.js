87bb745b40116958ad974382dcb41f54260631f7
node <ratafak15@gmail.com>
2024-07-17 20:21:35 -0400

    Algunos pequenos cambios

--- Fun.js
+++ Fun.js
@@ -1,19 +1,4 @@
-// const btn = document.getElementById('btnFetchCharacters');
-// const Insertar = document.getElementsByClassName(".Imagen");
 
-// btn.addEventListener('click', () => {
-//     fetch('https://rickandmortyapi.com/api/character')
-//       .then((Response) => Response.json())
-//       .then((data) => cargarPersonaje(data.results));
-
-// });
-
-
-//   function cargarPersonaje(character) {
-//     character.forEach(ch => {
-//         Insertar.innerHTML += `<img src="${ch.image}">`;
-//     })
-//   }
 
 const div = document.getElementById('contenedor');
 const btn = document.getElementById('btnFetchCharacters');
